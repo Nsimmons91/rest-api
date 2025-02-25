08ea65a41be964290476e6b1f9941c9b36c35ba1
Nina Simmons <pkpbabies@gmail.com>
2025-02-25 14:01:40 -0500

    Add autosave, update database name in db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import { Client } require ('pg'); //Clientfrom 'pg';
 const client = new Client({
   user: 'nsimmons91', // replace with your PostgreSQL username
   host: 'localhost',
-  database: 'booksdb',
+  database: 'mybooks',
   password: 'Snuffer3', // replace with your PostgreSQL password
   port: 5432,
 });
@@ -11,4 +11,4 @@ client.connect()
 .then(() => console.log('Connected to PostgreSQL'))
 .catch(err => console.error('Error connecting to PostgreSQL:', err.stack));
 
-module.exports = client;
+ export default client;
